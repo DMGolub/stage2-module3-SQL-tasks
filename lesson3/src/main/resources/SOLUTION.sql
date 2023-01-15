0d80285d5159ec21936bf15a1b535d77c17a8e44
Dmitry Golub <dmgolub@mail.ru>
2023-01-16 01:57:08 +0400

    Changes in some solutions because of failed tests.

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,10 +1,12 @@
 ALTER TABLE student ALTER COLUMN birthday SET NOT NULL;
 
 ALTER TABLE mark ALTER COLUMN student_id SET NOT NULL;
-ALTER TABLE payment ALTER COLUMN subject_id SET NOT NULL;
-ALTER TABLE payment ADD CONSTRAINT mark_value CHECK(mark >= 1 AND mark <= 10);
+ALTER TABLE mark ALTER COLUMN subject_id SET NOT NULL;
+ALTER TABLE mark ADD CONSTRAINT mark_value1 CHECK(mark >= 1);
+ALTER TABLE mark ADD CONSTRAINT mark_value2 CHECK(mark <= 10);
 
-ALTER TABLE subject ADD CONSTRAINT grade_value CHECK(grade >= 1 AND grade <= 5);
+ALTER TABLE subject ADD CONSTRAINT grade_value1 CHECK(grade >= 1);
+ALTER TABLE subject ADD CONSTRAINT grade_value2 CHECK(grade <= 5);
 
 ALTER TABLE paymenttype ADD CONSTRAINT unique_name UNIQUE(name);
 
--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,16 +1,5 @@
-SELECT s.* FROM student AS s
-JOIN mark AS m
-ON s.id = m.student_id
-GROUP BY s.id
-HAVING AVG(m.mark) > 8;
+SELECT s.* FROM student AS s JOIN mark AS m ON s.id = m.student_id GROUP BY s.id HAVING AVG(m.mark) > 8;
 
-SELECT s.id, s.name FROM student AS s
-JOIN mark AS m
-ON s.id = m.student_id
-GROUP BY s.id
-HAVING MIN(m.mark) > 7;
+SELECT s.id, s.name FROM student AS s JOIN mark AS m ON s.id = m.student_id GROUP BY s.id HAVING MIN(m.mark) > 7;
 
-SELECT DISTINCT s.id, s.name FROM student AS s
-JOIN payment AS p
-ON s.id = p.student_id
-WHERE (SELECT COUNT(*) FROM payment WHERE payment_date >= '2019-01-01' AND payment_date < '2020-01-01') > 2;
+SELECT DISTINCT s.id, s.name FROM student AS s JOIN payment AS p ON s.id = p.student_id WHERE (SELECT COUNT(*) FROM payment WHERE payment_date >= '2019-01-01' AND payment_date < '2020-01-01') > 2;
